00471098a08a74a022bf6ab3dd3d395e6a341d8b
AmithShetty12 <94440534+AmithShetty12@users.noreply.github.com>
2024-03-21 13:09:08 +0530

    Add files via upload

---
+++ Program 6/p6.cs
@@ -0,0 +1,24 @@
+using System;
+class program66
+{
+ public static void Main(string[] args)
+ {
+   Console.WriteLine("Enter the number of rows for floyd's triangle:");
+   int numrows=int.Parse(Console.ReadLine());
+   int[][] triangle=new int[numrows][];
+   int num=1;
+   Console.WriteLine();
+   for(int row=0;row<numrows;row++)
+   {
+    triangle[row]=new int[row+1];
+    for(int col=0;col<=row;col++)
+    {
+     triangle[row][col]=num;
+     Console.Write(triangle[row][col]+" ");
+     num++;
+    }
+    Console.WriteLine();
+   }
+   Console.ReadLine();
+ }
+}
